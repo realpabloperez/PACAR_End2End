d247bf009a6b7b713485b7b632f10dc1aa5e2c73
realpabloperez <145840452+realpabloperez@users.noreply.github.com>
2023-12-02 13:49:07 +0100

    cambios del formato del json con index + fecha como nombre de cada json

--- python/formatear.py
+++ python/formatear.py
@@ -7,20 +7,20 @@ with open("archivos/datos.json", "r") as file:
 
 records = data.get("results", [])
 
-output_root_directory = "output_json_files"
-os.makedirs(output_root_directory, exist_ok=True)
+output_directory = "output_json_files"
+os.makedirs(output_directory, exist_ok=True)
 
+# Split the original JSON file into multiple smaller JSON files
 for index, record in enumerate(records, start=1):
     # Extract the timestamp from the 'updated_at' field, assuming it's in the format '02/12/2023 11:10:15'
     timestamp_str = record.get("updated_at", "")
     timestamp = datetime.strptime(timestamp_str, "%d/%m/%Y %H:%M:%S").strftime("%Y%m%d%H%M%S")
 
-    # Create a directory with the date (e.g., 20231202) if it doesn't exist
-    output_date_directory = os.path.join(output_root_directory, timestamp[:8])
-    os.makedirs(output_date_directory, exist_ok=True)
+    # Create a filename with the timestamp, e.g., record_20231202111015.json
+    output_filename = os.path.join(output_directory, f'{index}_{timestamp}.json')
 
-    # Create a filename inside the date directory, e.g., record_20231202_1.json
-    output_filename = os.path.join(output_date_directory, f'record_{timestamp}_{index}.json')
-
+    # Save each record individually
     with open(output_filename, 'w') as output_file:
         json.dump(record, output_file, indent=2)
+
+print(f"{len(records)} records have been split into individual JSON files.")
