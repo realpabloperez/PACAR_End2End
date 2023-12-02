87e3e279007a825df4edd559244517a6dafe4e77
carlosferrersanchez <cfs260501@gmail.com>
2023-12-02 12:54:36 +0100

    ingestion nuevo

--- python/ingestion.py
+++ python/ingestion.py
@@ -1,18 +1,26 @@
 import json
 from pymongo import MongoClient 
+import os
 
 myclient = MongoClient("mongodb://root:example@localhost:27017/?authSource=admin")
 
 base_de_datos = myclient.Valenbisi
 collection = base_de_datos["Paradas de Valenbisi"]
 
+i = 1
+while True:
+    file_path = f'output_json_files/record_{i}.json'
+    if os.path.exists(file_path):
+        with open(file_path) as file:
+            file_data = json.load(file)
+            
+            if isinstance(file_data, list):
+                collection.insert_many(file_data) 
+            else:
+                collection.insert_one(file_data)
+                
+        i += 1
+    else:
+        break
 
-for i in range(1,101):
-	with open(f'output_json_files/record_{i}.json') as file:
-		file_data = json.load(file)
-		
-	if isinstance(file_data, list):
-		collection.insert_many(file_data) 
-	else:
-		collection.insert_one(file_data)
 
