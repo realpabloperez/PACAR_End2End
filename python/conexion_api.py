9e303883cd67c697b166e5b37dda9b46a717acb4
realpabloperez <145840452+realpabloperez@users.noreply.github.com>
2023-12-02 10:23:47 +0100

    correciones API

--- python/conexion_api.py
+++ python/conexion_api.py
@@ -1,7 +1,7 @@
 import urllib.request
 import json
 
-url = "https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/valenbisi-disponibilitat-valenbisi-dsiponibilidad/records?"
+url = "https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/valenbisi-disponibilitat-valenbisi-dsiponibilidad/records?limit=100"
 
 response = urllib.request.urlopen(url)
 
@@ -9,3 +9,7 @@ data = response.read().decode('UTF-8')
 
 file = open("archivos/datos.json", "w")
 file.write(data)
+
+new_data = json.loads(data)
+get_articles = new_data['results']
+new_data_articles = json.dumps(get_articles)
